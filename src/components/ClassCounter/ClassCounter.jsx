638e1239d12cdb7edf1aca4c4ac346beac6c4d5d
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-01-08 12:53:55 +0100

    Done with class counter.

--- src/components/ClassCounter/ClassCounter.jsx
+++ src/components/ClassCounter/ClassCounter.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export class ClassCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    let { count } = this.state;
+    this.setState({ count: count + 1 })
+  }
+
   render() {
+    const { count } = this.state;
+
     return (
       <div>
-        
+        <button onClick={() => this.incrementCount() }>Count: { count }</button>
       </div>
     )
   }
