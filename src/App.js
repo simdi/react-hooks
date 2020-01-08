71cdcfede971795172b8b5e0fdd9a5e5f72b03a0
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-01-08 13:05:28 +0100

    Done with HookCounter.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ClassCounter from './components/ClassCounter/ClassCounter';
+import HookCounter from './components/HookCounter/HookCounter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <ClassCounter />
+      <br />
+      <HookCounter />
     </div>
   );
 }
