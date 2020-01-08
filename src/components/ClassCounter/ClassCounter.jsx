9df508fa82d3ef7162c0eee2446735d3f1cd8c2b
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-01-08 12:36:05 +0100

    first commit

---
+++ src/components/ClassCounter/ClassCounter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class ClassCounter extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ClassCounter
